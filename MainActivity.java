ca0767c387c42abfd0ba595678d7f97dcb80aaa8
drorifar <drorifar@gmail.com>
2013-10-19 19:50:26 +0300

    My First App2

--- MainActivity.java
+++ MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends Activity {
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-        
+
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
